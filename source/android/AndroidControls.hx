1cfdde419a4e14d40f830e1b4c0d2267c7680031
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-07-05 11:09:50 +0300

    Update AndroidControls.hx

--- source/android/AndroidControls.hx
+++ source/android/AndroidControls.hx
@@ -187,7 +187,6 @@ class Menu extends MusicBeatState
 		curSelected = config.getcontrolmode();
 
 		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
-		bg.scrollFactor.set(0, yScroll);
 		bg.setGraphicSize(Std.int(bg.width * 1.175));
 		bg.updateHitbox();
 		bg.screenCenter();
